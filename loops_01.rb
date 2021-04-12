9328a4d1e0a1f3decae8eef51d874682e64fe11f
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-12 10:46:42 +0200

    Ajout loops_01.rb

---
+++ loops_01.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Ok, c'est cool mais testons une nouvelle méthode."
+5.times do
+  puts "Bonjour, monde !"
+end
