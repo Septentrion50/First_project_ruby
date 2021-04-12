331e1cb7a50d3faf2a38a73af5fa57355c3c3bdf
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-12 12:04:41 +0200

    Ajout de exo_05.rb

---
+++ exos/exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" # La syntaxe #{} est une string formattée
+puts "Travail : #{10 * 5 * 11}" # Sort le nombre d'heures de travail attendues sur THP
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Convertit le résultat précédent en minutes et l'affiche dans une string
+
+puts "Et en secondes ?" # Imprime "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 # Sort le résultat en secondes
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # Non
+
+puts 3 + 2 < 5 - 7 # false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # Affiche "Ca fait combien 3 + 2 ? 5"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # Affiche "Ca fait combien 5 - 7 ? -2"
+
+puts "Ok, c'est faux alors !" # Affiche "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :" # Affiche "C'est drôle ça faisons en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # Affiche "Est-ce que 5 est plus grand que -2 ? true"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # Affiche "Est-ce que 5 est supérieur ou égal à -2 ? true"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # Affiche "Est-ce que 5 est inférieur ou égal à -2 ? false"
