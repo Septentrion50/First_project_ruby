7348b17baa88080076c2c0972e9a0e209123586c
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-12 10:43:32 +0200

    Ajout du fichier conditions_01.rb

---
+++ conditions_01.rb
@@ -0,0 +1,16 @@
+# Déclaratoin d'un age égal à 21
+
+age = 21
+
+# vérifie que l'age est strictement supérieur à 17
+
+if age > 17
+  # Si c'est le cas
+  puts "Cette personne est majeure !"
+end
+
+# Vérifie que l'age est égal à 21
+
+if age == 21
+  puts "Cette personne a 21 ans !"
+end
