b1a2e0f0988649ad3be84d358fc3ad1d0ff6a61c
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-12 12:16:47 +0200

    Ajout de exo_07a, b et c

---
+++ exos/exo_07a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp # gets.chomp demande une entrée à l'utilisateur
+puts user_name
---
+++ exos/exo_07b.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp # gets.chomp demande une entrée à l'utilisateur
+puts user_name
+
